886517938bb26609cdc33edb0f79d6a54396959b
Dewapathi <pradeepa@creatit.com.au>
2024-03-17 14:21:39 +0530

    Added secret key and client id again.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,10 +8,10 @@ import bcrypt from 'bcrypt';
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
-        // GoogleProvider({
-        //     clientId: process.env.GOOGLE_CLIENT_ID as string,
-        //     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        // }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
         CredentialsProvider({
             name: 'credentials',
             credentials: {
